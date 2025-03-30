406d443c21c1ae643b0a5aef5bf3bd4bb2e9eefb
joeheapy <joe.heapy@me.com>
2025-03-30 20:50:11 +0100

    First Deploy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import time
-import webbrowser
 import threading
 from http.server import HTTPServer
 from lateral_thinking import LateralThinkingEnhanced
@@ -15,26 +14,41 @@ def main():
     def handler_factory(*args, **kwargs):
         return FormHandler(*args, analyzer=analyzer, **kwargs)
     
-    # Start HTTP server
-    def run_server():
-        server = HTTPServer(('localhost', 8000), handler_factory)
-        print('Starting server at http://localhost:8000')
+    # Get port from environment variable or use default
+    port = int(os.environ.get('PORT', 8000))
+    
+    # Check if running in production (Render sets this environment variable)
+    is_production = os.environ.get('RENDER', False)
+    
+    if is_production:
+        # Production mode: bind to all interfaces
+        host = '0.0.0.0'
+        server = HTTPServer((host, port), handler_factory)
+        print(f'Starting server at http://{host}:{port} (production mode)')
         server.serve_forever()
-    
-    # Start server in a separate thread
-    server_thread = threading.Thread(target=run_server)
-    server_thread.daemon = True
-    server_thread.start()
-    
-    # Open the initial report in the browser
-    webbrowser.open('http://localhost:8000')
-    
-    # Keep main thread running
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("\nShutting down...")
+    else:
+        # Development mode: run in thread and open browser
+        host = 'localhost'
+        
+        def run_server():
+            server = HTTPServer((host, port), handler_factory)
+            print(f'Starting server at http://{host}:{port} (development mode)')
+            server.serve_forever()
+        
+        server_thread = threading.Thread(target=run_server)
+        server_thread.daemon = True
+        server_thread.start()
+        
+        # Open the initial report in the browser (dev only)
+        import webbrowser
+        webbrowser.open(f'http://{host}:{port}')
+        
+        # Keep main thread running in development
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print("\nShutting down...")
 
 if __name__ == "__main__":
     main()
